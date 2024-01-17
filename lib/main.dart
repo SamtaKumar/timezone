260c809c15c9908d71f435b6e75506db30a59bfd
Hash-himadri <hashcodes324@gmail.com>
2024-01-17 13:56:38 +0530

    updated by Himadrie

--- lib/main.dart
+++ lib/main.dart
@@ -3,16 +3,16 @@ import 'package:timezone/Pages/loading.dart';
 import 'package:timezone/Pages/home.dart';
 import 'package:timezone/Pages/choose_location.dart';
 
+//Commented by Himadrie
 void main() {
   runApp(MaterialApp(
     //home: ChooseLocation(),
     //home: Loading(),
     initialRoute: "/",
     routes: {
-      "/" : (context) => Loading(),
-      "/home" : (context) => const Home(),
-      "/location" : (context) => const ChooseLocation(),
+      "/": (context) => Loading(),
+      "/home": (context) => const Home(),
+      "/location": (context) => const ChooseLocation(),
     },
   ));
 }
-
